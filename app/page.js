62b9bfc7a4047806689f38b4a8a408c3919412b0
sayandh raj <sayandhsr123@gmail.com>
2026-03-28 14:43:11 +0530

    test: replacing root redirect with static HTML

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
-export default function RootPage() {
-  redirect("/dashboard");
+export default function Home() {
+  return <div>HOME WORKING</div>;
 }
